33d53d36465ddf1847b3951dde7c7ba4ca5de99b
Aneesh P U <aneeshpu@gmail.com>
2011-08-21 12:18:38 +0530

    Displays messages

--- src/com/vanhelsing/Inbox.java
+++ src/com/vanhelsing/Inbox.java
@@ -1,13 +1,22 @@
 package com.vanhelsing;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
 
-public class Inbox extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+public class Inbox extends ListActivity {
+    private static final String SMS_CONTENT_URI = "content://sms/inbox";
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        Cursor cursor = managedQuery(Uri.parse(SMS_CONTENT_URI), new String[] {"_id", "body"}, null, null, "date DESC");
+		ListAdapter listAdapter = new SimpleCursorAdapter(this, R.layout.main, cursor, new String[]{"body"}, new int[] {R.id.message});
+		
+		setListAdapter(listAdapter);
     }
 }

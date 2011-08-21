f4c7521d6c80b4de03c014267680afe70350c58d
Aneesh P U <aneeshpu@gmail.com>
2011-08-21 17:53:10 +0530

    For some weird reason what I tried first has started working now. weird weird android

--- src/com/vanhelsing/Inbox.java
+++ src/com/vanhelsing/Inbox.java
@@ -6,15 +6,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnLongClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.LinearLayout;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Inbox extends ListActivity {
 	private static final String SMS_CONTENT_URI = "content://sms/inbox";
@@ -30,26 +28,23 @@ public class Inbox extends ListActivity {
 		ListView listView = getListView();
 		listView.setTextFilterEnabled(true);
 
-		listView.setOnLongClickListener(new OnLongClickListener() {
+		listView.setOnItemLongClickListener(new OnItemLongClickListener() {
 
 			@Override
-			public boolean onLongClick(View message) {
-
-				Log.i("aneesh", "aneesh has long clicked the message");
-				TextView messageBox = (TextView) message;
-				Toast.makeText(getApplicationContext(), messageBox.getText(), Toast.LENGTH_SHORT);
+			public boolean onItemLongClick(AdapterView<?> arg0, View view, int position, long id) {
+				Log.i("aneesh", "aneesh long pressed message: " + ((TextView)view).getText());
 				return true;
 			}
+
 		});
 
 		listView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> listView, View messageView, int position, long id) {
-				TextView message = (TextView) messageView;
-				Log.i("aneesh", "aneesh textview == null ? " + (message == null) + " position = " + position);
-				Toast.makeText(getApplicationContext(), message.getText(), Toast.LENGTH_SHORT).show();
+			public void onItemClick(AdapterView<?> listView, View view, int position, long id) {
+				Log.i("aneesh", "aneesh clicked message: " + ((TextView)view).getText());
 			}
 		});
+
 	}
 }

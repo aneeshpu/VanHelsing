578fa888ca13d7404a70c682addcd1b871fdba05
Aneesh P U <aneeshpu@gmail.com>
2011-08-23 12:54:22 +0530

    Implemented a walking skeleton for training the classifier with a spam message

--- src/com/vanhelsing/Inbox.java
+++ src/com/vanhelsing/Inbox.java
@@ -1,5 +1,7 @@
 package com.vanhelsing;
 
+import com.vanhelsing.brain.Classifier;
+
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.net.Uri;
@@ -8,8 +10,10 @@ import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -31,23 +35,34 @@ public class Inbox extends ListActivity {
 		listView.setTextFilterEnabled(true);
 
 		registerForContextMenu(listView);
-		
 
 	}
-	
+
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
-		
-		AdapterView.AdapterContextMenuInfo contextMenuInfo = (AdapterView.AdapterContextMenuInfo)menuInfo; 
-		LinearLayout linearLayout = (LinearLayout)contextMenuInfo.targetView;
-		
-		TextView textView = (TextView)linearLayout.getChildAt(0);
-		
+
+		AdapterView.AdapterContextMenuInfo contextMenuInfo = (AdapterView.AdapterContextMenuInfo) menuInfo;
+		LinearLayout linearLayout = (LinearLayout) contextMenuInfo.targetView;
+
+		TextView textView = (TextView) linearLayout.getChildAt(0);
+
 		Log.i("aneesh", "aneesh long pressed message: " + (textView.getText()));
-		
+
 		menu.setHeaderTitle("Spam picker");
 		MenuInflater menuInflater = getMenuInflater();
 		menuInflater.inflate(R.menu.inbox_context, menu);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo contextMenuInfo = (AdapterContextMenuInfo) item.getMenuInfo();
+		
+		LinearLayout targetView = (LinearLayout) contextMenuInfo.targetView;
+		
+		TextView textView = (TextView) targetView.getChildAt(0);
+		new Classifier().markAsSpam((String)textView.getText());
+		return true;
+
 	}
 }
---
+++ src/com/vanhelsing/brain/Classifier.java
@@ -0,0 +1,11 @@
+package com.vanhelsing.brain;
+
+import android.util.Log;
+
+public class Classifier {
+
+	public void markAsSpam(String message) {
+		Log.i("vanhelsing", "vanhelsin marking " + message + " as spam");
+	}
+
+}
